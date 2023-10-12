cc1e6893553aa8c06af583fdcaaa93279c149621
Henrique Martins <hiikemartins@gmail.com>
2023-10-11 23:22:50 -0300

    Feat: Create decorator

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Param,
   Post,
   UsePipes,
   ValidationPipe,
@@ -10,18 +9,20 @@ import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { Roles } from 'src/decorators/roles.decorators';
 import { Usertype } from 'src/enum/user-type.enum';
+import { UserId } from 'src/decorators/user-id.decorator';
 
 @Roles(Usertype.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Post('/:userId')
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDto: CreateAddressDto,
-    @Param('userId') userId: number,
+    @UserId() userId: number,
   ) {
+    console.log('userId', userId);
     return this.addressService.createAddress(createAddressDto, userId);
   }
 }
